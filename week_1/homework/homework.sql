3b93b070b95649d9f3ead88e6d7545cb17ef4aed
blessedmadukoma <blessedmadukoma@gmail.com>
2024-01-19 00:40:32 +0100

    week 1: submitted homework

--- week_1/homework/homework.sql
+++ week_1/homework/homework.sql
@@ -1,42 +1,93 @@
+-- SQL Homework section
+
+-- Question 3: Count records
+-- How many taxi trips were totally made on September 18th 2019?
+
+-- Tip: started and finished on 2019-09-18.
+
+-- Remember that lpep_pickup_datetime and lpep_dropoff_datetime columns are in the format timestamp (date and hour+min+sec) and not in date.
+
+SELECT COUNT(*)
+FROM hw_green_trip_data
+WHERE
+    CAST(lpep_pickup_datetime AS DATE) = '2019-09-18'
+    AND CAST(lpep_dropoff_datetime AS DATE) = '2019-09-18';
+
+-- OR
+
+SELECT COUNT(*)
+FROM hw_green_trip_data
+WHERE DATE(lpep_pickup_datetime) = '2019-09-18' AND DATE(lpep_dropoff_datetime) = '2019-09-18';
+
+-- Question 4: Which was the day with the largest trip distance? (Choose one)
+
 SELECT
-	*
+    DATE_TRUNC('day', lpep_pickup_datetime) AS pickup_day,
+    SUM(trip_distance) AS total_trip_distance
 FROM
-	taxi_zone_data
--- 	green_trip_data
-LIMIT 100;
+    hw_green_trip_data
+GROUP BY
+    pickup_day
+ORDER BY
+    total_trip_distance DESC
+LIMIT 1;
 
--- SELECT COUNT(*)
--- FROM green_trip_data
--- WHERE
---     DATE_PART('MONTH', lpep_pickup_datetime) = 1
---     AND DATE_PART('DAY', lpep_pickup_datetime) = 15;
+-- Question 5: Three biggest pick up Boroughs
+-- Consider lpep_pickup_datetime in '2019-09-18' and ignoring Borough has Unknown
 
--- Which was the day with the largest trip distance? (Choose one)
--- SELECT
--- 	lpep_pickup_datetime, lpep_dropoff_datetime, trip_distance
--- FROM
--- 	green_trip_data
--- GROUP BY trip_distance
--- LIMIT 5;
+-- Which were the 3 pick up Boroughs that had a sum of total_amount superior to 50000?
+-- - "Brooklyn" "Manhattan" "Queens" [Answer]
+-- - "Bronx" "Brooklyn" "Manhattan"
+-- - "Bronx" "Manhattan" "Queens"
+-- - "Brooklyn" "Queens" "Staten Island"
 
--- SELECT COUNT(*)
--- FROM green_trip_data
--- WHERE
---     TO_CHAR(lpep_pickup_datetime, 'MM') = '01'
---     AND TO_CHAR(lpep_pickup_datetime, 'DD') = '15';
+SELECT
+    tzd."Borough",
+    SUM(gtd.total_amount) AS total_amount_sum
+FROM
+    hw_green_trip_data gtd
+JOIN
+    hw_taxi_zone_data tzd ON gtd."PULocationID" = tzd."LocationID"
+WHERE
+    -- gtd.lpep_pickup_datetime >= '2019-09-18'
+    -- AND gtd.lpep_pickup_datetime < '2019-09-19'
+    DATE(gtd.lpep_pickup_datetime) >= '2019-09-18'
+    AND DATE(gtd.lpep_pickup_datetime) < '2019-09-19'
+GROUP BY
+    tzd."Borough"
+HAVING
+    SUM(gtd.total_amount) > 50000
+ORDER BY
+    total_amount_sum DESC
+LIMIT 3;
 
--- SELECT
---     DATE_TRUNC('day', CAST(lpep_pickup_datetime AS timestamp)) AS trip_day,
---     SUM(trip_distance) AS total_trip_distance
--- FROM
---     green_trip_data
--- GROUP BY
---     trip_day
--- ORDER BY
---     total_trip_distance DESC
--- -- LIMIT 1;
+-- Question 6. Largest tip
+-- For the passengers picked up in the Astoria Zone 
+-- 	which was the drop off zone that had the largest tip? 
+-- 	We want the name of the zone, not the id. (Choose one)
 
--- Question 5. The number of passengers
+SELECT
+    gtd."PULocationID" AS pickup_location_id,
+    tzd1."Zone" AS pickup_zone,
+    gtd."DOLocationID" AS dropoff_location_id,
+    tzd2."Zone" AS dropoff_zone,
+    MAX(gtd."tip_amount") AS max_tip_amount
+FROM
+    hw_green_trip_data gtd
+JOIN
+    hw_taxi_zone_data tzd1 ON gtd."PULocationID" = tzd1."LocationID"
+JOIN
+    taxi_zone_data tzd2 ON gtd."DOLocationID" = tzd2."LocationID"
+WHERE
+    tzd1."Zone" = 'Astoria'
+GROUP BY
+    gtd."PULocationID", tzd1."Zone", gtd."DOLocationID", tzd2."Zone"
+ORDER BY
+    max_tip_amount DESC
+LIMIT 1;
+
+
+-- Zoomcamp 2022 Question 5. The number of passengers
 -- In 2019-01-01 how many trips had 2 and 3 passengers? (Choose one)
 -- SELECT
 --     DATE_TRUNC('day', CAST(lpep_pickup_datetime AS timestamp)) AS trip_day,
@@ -49,43 +100,3 @@ LIMIT 100;
 -- GROUP BY
 --     trip_day
 -- LIMIT 1;
-
-
--- Question 6. Largest tip
--- For the passengers picked up in the Astoria Zone 
--- 	which was the drop off zone that had the largest tip? 
--- 	We want the name of the zone, not the id. (Choose one)
-
-SELECT
--- 	tzd."LocationID" as TZD_LocationID,
--- 	tzd."Zone" as TZD_Zone,
--- 	tzd."service_zone" as TZD_Service_Zone
-*
-from taxi_zone_data as tzd
-inner join green_trip_data as gtd
-ON tzd."LocationID" = gtd."PULocationID"
-where gtd."tip_amount" > 0 AND tzd."Zone" = 'Astoria'
--- where gtd."tip_amount" > 0
-ORDER BY gtd."tip_amount" DESC
-LIMIT 10;
-
-SELECT
-    gtd."PULocationID" AS pickup_location_id,
-    tzd1."Zone" AS pickup_zone,
-    gtd."DOLocationID" AS dropoff_location_id,
-    tzd2."Zone" AS dropoff_zone,
-    MAX(gtd."tip_amount") AS max_tip_amount
-FROM
-    green_trip_data gtd
-JOIN
-    taxi_zone_data tzd1 ON gtd."PULocationID" = tzd1."LocationID"
-JOIN
-    taxi_zone_data tzd2 ON gtd."DOLocationID" = tzd2."LocationID"
-WHERE
-    tzd1."Zone" = 'Astoria'
-GROUP BY
-    gtd."PULocationID", tzd1."Zone", gtd."DOLocationID", tzd2."Zone"
-ORDER BY
-    max_tip_amount DESC
-LIMIT 1;
-
